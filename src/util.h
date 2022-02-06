a709520695851289e05d260e35b855030bcde29f
Luna Nothard <l.nothard@lancaster.ac.uk>
2022-02-06 14:13:47 +0000

    continued refactor

---
+++ src/util.h
@@ -0,0 +1,32 @@
+//
+// Created by Luna Nothard on 06/02/2022.
+//
+
+#ifndef WARZONE2100_UTIL_H
+#define WARZONE2100_UTIL_H
+
+#include <3rdparty/boost-preprocessor
+
+
+#define X_DEFINE_ENUM_WITH_STRING_CONVERSIONS_TOSTRING_CASE(r, data, elem)    \
+    case elem : return BOOST_PP_STRINGIZE(elem);
+
+#define DEFINE_ENUM_WITH_STRING_CONVERSIONS(name, enumerators)                \
+    enum name {                                                               \
+        BOOST_PP_SEQ_ENUM(enumerators)                                        \
+    };                                                                        \
+                                                                              \
+    inline const char* ToString(name v)                                       \
+    {                                                                         \
+        switch (v)                                                            \
+        {                                                                     \
+            BOOST_PP_SEQ_FOR_EACH(                                            \
+                X_DEFINE_ENUM_WITH_STRING_CONVERSIONS_TOSTRING_CASE,          \
+                name,                                                         \
+                enumerators                                                   \
+            )                                                                 \
+            default: return "[Unknown " BOOST_PP_STRINGIZE(name) "]";         \
+        }                                                                     \
+    }
+
+#endif//WARZONE2100_UTIL_H
